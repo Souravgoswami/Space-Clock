1480d8089535290472a5f7e8f951536a0b821545
Sourav Goswami <37468605+Souravgoswami@users.noreply.github.com>
2018-10-25 08:44:23 +0530

    Update about.rb

--- Space::Clock/Subwindows/about.rb
+++ Space::Clock/Subwindows/about.rb
@@ -1,20 +1,21 @@
 #!/usr/bin/env ruby
 require 'ruby2d'
-
 $width, $height = 1200, 840
-def main()
-	set title: 'About Space::Clock', background: 'black', width: $width, height: $height, resizable: true
+def main
+	set title: 'About Space::Clock', background: 'black', width: $width, height: $height, resizable: true, fps_cap: 30
 	bg = Rectangle.new width: $width, height: $height - 28, z: -10, color: %w(purple black black black) ; bg.opacity = 0.4
 	file = File.open('instruction.txt').readlines
 	file = file[11..-1]
-	l, s, stars = 0, {}, {}
+	l, s, stars = 0, [], []
 	10.times do |temp|
-		s.merge! temp => c = Square.new(x: rand(0..$width), y: rand($height .. $height + 1500), z: -5, size: rand(60..90)) ; c.opacity = rand(0.1..0.3)
+		size = rand(60..90)
+		s << c = Image.new('crystals/1pixel_square.png', x: rand(0..$width), y: rand($height .. $height + 1500), z: -5, width: size, height: size)
+		c.opacity = rand(0.1..0.3)
 	end
 
 	50.times do |temp|
 		tempsize = rand(8..15)
-		stars[temp] =Image.new(path: ['crystals/hoverstars.png', 'crystals/hoverstars1.png', 'crystals/hoverstars2.png', 'crystals/hoverstars3.png',
+		stars << Image.new( ['crystals/hoverstars.png', 'crystals/hoverstars1.png', 'crystals/hoverstars2.png', 'crystals/hoverstars3.png',
 					'crystals/hoverstars4.png', 'crystals/hoverstars5.png', 'crystals/hoverstars6.png', 'crystals/hoverstars7.png',
 					'crystals/hoverstars8.png', 'crystals/hoverstars9.png'].sample, x: rand(0..$width), y: rand(0..$height), width: tempsize, height: tempsize)
 	end
@@ -24,21 +25,21 @@ def main()
 	chash = {}
 	($width/30).times do |temp|
 		tempsize = rand(20..50)
-		chash[temp] = Image.new(path: 'crystals/crystal3.png', x: rand(0..$width), y: $height - tempsize - 29, z: 1, width: tempsize, height: tempsize,color: '#232323')
+		chash[temp] = Image.new( 'crystals/crystal3.png', x: rand(0..$width), y: $height - tempsize - 29, z: 1, width: tempsize, height: tempsize,color: '#232323')
 	end
-	moon, mooncolor, moonbool = Image.new(path: 'crystals/moon.png', width: 100, height: 100, y: $height - 100),	mooncolor = [1,1,1,1], false
-	blackhole = Image.new(path: 'crystals/blackhole.png', x: rand(100..$width - 100), y: rand(100..$height - 100), z: -10,
+	moon, mooncolor, moonbool = Image.new( 'crystals/moon.png', width: 100, height: 100, y: $height - 100),	mooncolor = [1,1,1,1], false
+	blackhole = Image.new( 'crystals/blackhole.png', x: rand(100..$width - 100), y: rand(100..$height - 100), z: -10,
 		color: %w(orange blue fuchsia purple yellow green).sample)
 
 	text = {}
 	for line in file
-		text[l] = Text.new font: 'mage/MateSC-Regular.ttf', text: line.chomp, x: 5, y: l * 21, color: 'white', size: 17
+		text[l] = Text.new line.chomp, font: 'mage/MateSC-Regular.ttf', x: 5, y: l * 21, color: 'white', size: 17
 		l += 1
 	end
 
 	i, block = -1, {}
 	($width/76).times do |temp|
-		block[temp] = Image.new x: i * 89, y: $height - 30, path: 'crystals/block.png', width: 89
+		block[temp] = Image.new 'crystals/block.png', x: i * 89, y: $height - 30, width: 89
 		i += 1
 	end
 	Line.new x1: 0, x2: $width, y1: $height - 25, y2: $height - 25, width: 8, color: 'white'
@@ -48,8 +49,10 @@ def main()
 		cstars[temp] = Square.new x: rand(0..$width), y: rand($height - 17..$height - 1), color:  %w(yellow white #6ba3ff).sample, z: -5, size: 1
 	end
 
+	on :key_down do |k| exit! 0 if %w(escape return p q).include?(k.key) end
+
 	on :mouse_move do |e|
-		for val in s.values do val.opacity = 0 if val.contains?(e.x, e.y) end
+		for val in s do val.opacity = 0 if val.contains?(e.x, e.y) end
 		for val in text.values
 			if val.contains?(e.x, e.y) then val.color = 'yellow'  else val.color = 'white' end
 			if val.text.include?('www') and val.contains?(e.x, e.y) then val.color = 'red' end
@@ -78,6 +81,7 @@ def main()
 	end
 
 	update do
+		blackhole.rotate += 0.1
 		if moonbool
 			moon.color = mooncolor
 			mooncolor[1] -= 0.015 if mooncolor[1] > 0
@@ -85,7 +89,8 @@ def main()
 			moon.color = mooncolor
 			mooncolor[1] += 0.015 if mooncolor[1] < 1
 		end
-		for val in s.values do
+		s.each do |val|
+			val.rotate += val.width/250.0
 			unless val.y < -val.height or val.opacity <= 0 then val.y -= 1
 				else val.x, val.y, val.opacity = rand(0..$width), rand($height .. $height + 1500), rand(0.1..0.3) end
 		end
@@ -110,10 +115,10 @@ def main()
 		end
 
 		if Time.now.strftime("%N")[0].to_i % 2 == 0
-			stars.values.sample.opacity = [0, 1].sample
+			stars.sample.opacity = [0, 1].sample
 			block.values.sample.color = 'random'
 		end
 	end
 	show
 end
-main()
+main
